f13d879106a4f886f1ddc9c3a31b27904683862f
abhraneel3 <nisha.blog.author@gmail.com>
2024-07-10 10:58:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,8 +105,7 @@ async def fetch_and_send_data():
                 # Send custom invitation message
                 invitation_message = (
                     f"📢 Don't miss out on daily updates from our Telegram channel!\n\n"
-                    f"➡️ Join now for the latest government job opportunities and updates.\n\n"
-                    f"👉 Join us here: {invite_link}.\n\n"
+                    f"➡️ Follow the latest government job opportunities and updates.\n\n"
                     f"🔗 Invite your friends who are seeking government jobs to join us too!"
                 )
                 await send_message(bot, channel_id, invitation_message)
